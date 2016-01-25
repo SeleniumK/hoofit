cade1fb1e1fbdc1f877c78fc2521f423d5aca3bb
Amy Leek <amyeleek@gmail.com>
2016-01-25 13:47:19 -0800

    Finishing JSON, alpha markers

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,9 @@
 (function(module){
   var map = {};
+  var gMap;
 
   map.initMap = function(){
-   var gMap = new google.maps.Map(document.getElementById('map'), {
+    gMap = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 47.6097, lng: -122.3331},
       zoom: 12
     });
@@ -11,6 +12,15 @@
   };
 
 
+  map.setMarker = function(){
+    var mark = new google.maps.Marker({
+      position: {lat: 47.5798175, lng: -122.3268311},
+      map: gMap,
+      title: '5th Ave S & S Jackson St'
+    });
+
+    mark.setMap(gMap);
+  };
 
   module.map = map;
 })(window);
