52cfd4708612f0202d49ce1aede6aa174368a8c1
Sawako Ishida <sawako.yoriko@gmail.com>
2016-01-25 22:34:17 -0800

    added eventlistener and grabbing data from fields

--- scripts/mapplugin.js
+++ scripts/mapplugin.js
@@ -1,11 +1,15 @@
 (function(module){
   var DirectionsService = {};
   DirectionsService.route = function (callback){
-  var userStart = document.getElementById('startpoint').value;
-  var userEnd = document.getElementById('endpoint').value;
-  console.log(userStart);
-  console.log(userEnd);
+    var submitButton = document.getElementById('submitinfo');
+    submitButton.addEventListener('click', function(event){
+      event.preventDefault();
+      var userStart = document.getElementById('startpoint').value;
+      var userEnd = document.getElementById('endpoint').value;
+      console.log(userStart);
+      console.log(userEnd);
+    })
   };
-
+  DirectionsService.route();
   module.DirectionsService = DirectionsService;
 })(window);
