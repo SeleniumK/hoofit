8edb7e1b072f2ae10aa2aa6dd8f24e8ecdc865ab
Sawako Ishida <sawako.yoriko@gmail.com>
2016-01-26 10:08:44 -0800

    user data in request obj.

--- scripts/mapplugin.js
+++ scripts/mapplugin.js
@@ -1,15 +1,38 @@
 (function(module){
+  //DirectionsService object
   var DirectionsService = {};
+
   DirectionsService.route = function (callback){
+    //submit button with click eventlistener
     var submitButton = document.getElementById('submitinfo');
     submitButton.addEventListener('click', function(event){
       event.preventDefault();
-      var userStart = document.getElementById('startpoint').value;
-      var userEnd = document.getElementById('endpoint').value;
-      console.log(userStart);
-      console.log(userEnd);
+
+      //user request object
+      var request = {
+        origin: '',
+        destination: '',
+        travelMode: ''
+      }
+      //grabing user input and pushing data to request object (to line 10-14)
+      request.origin = document.getElementById('startpoint').value;
+      request.destination = document.getElementById('endpoint').value;
+      request.travelMode = 'walking';
+      console.log(request);
+      var directionsDisplay = new google.maps.DirectionsRenderer({
+        map: map
+      });
+      var directionsService = new google.maps.DirectionsService();
+      directionsService.route(request, function(response, status){
+        if(status == google.maps.DirectionStatus.OK){
+          directionsDisplay.setDirections(response);
+        }
+      });
     })
   };
+  //just for now twsting purpose
   DirectionsService.route();
+
+  //module
   module.DirectionsService = DirectionsService;
 })(window);
