63fbcc30e5c32f167c2a43a71535adc4da79bab6
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-01-27 16:10:12 -0800

    made tabs, and selected tab in nav bar, more visible

--- scripts/pageView.js
+++ scripts/pageView.js
@@ -1,12 +1,19 @@
 (function(module){
   var pageView = {};
+  pageView.clickEvents = function(){
+    $('.icon-menu3').click(function(e){
+      $('#menu li').toggle();
+    });
+    $('#menu a').click(function(e){
+      $('#menu a').removeClass('currentpage');
+      $(this).addClass('currentpage');
+    });
+  };
 
   pageView.initPage = function(activePage){
     $('nav').show();
     $('#' + activePage).show().siblings().hide();
-    $('.icon-menu3').click(function(e){
-      $('#menu li').toggle();
-    })
+    pageView.clickEvents();
   };
 
   module.pageView = pageView;
