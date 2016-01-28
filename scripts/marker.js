e953925e64caa73e239408fe52b8db1a4bff3e62
Amy Leek <amyeleek@gmail.com>
2016-01-27 20:50:37 -0800

    Working icons, some questions about how to handle edge cases

--- scripts/marker.js
+++ scripts/marker.js
@@ -5,7 +5,26 @@
 
   //this just pushes to Marker.all. You will need to call clearMakers before using this
   Marker.loadMarker = function(mark){
-    Marker.all.push(new google.maps.Marker(mark));
+    var image,
+        audio = 'images/audible_logo.png',
+        vibrate = 'images/vibrate_logo.png',
+        tactile = 'images/tactile_logo.png';
+
+    //or I could put an extra field in the JSON. I don't know.
+    //how do we handle multiple types? All are audible, but some are all three and some aren't...
+    if(mark.type.length < 2){
+      image = audio;
+    }else if(mark.type[1] == 'vibrate'){
+      image = vibrate;
+    }else{
+      image = tactile;
+    }
+
+    Marker.all.push(new google.maps.Marker({
+      position: mark.position,
+      title: mark.title,
+      icon:image
+    }));
   };
 
   //this needs refactoring, since it only works for these accessible markers
@@ -28,6 +47,12 @@
 
   //clear out the already set markers
   Marker.clearMarkers = function(){
+    //unset them from the map
+    Marker.all.forEach(function(mark){
+      mark.setMap(null);
+    });
+
+    //and clear the array
     Marker.all = [];
   };
 
