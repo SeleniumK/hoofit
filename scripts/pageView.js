ea3728ab0314a4790052060c0b6f84b550e8ce6d
Willie Richardson <willie.richardson72@gmail.com>
2016-01-27 16:24:46 -0800

    edited hamburger menu

--- scripts/pageView.js
+++ scripts/pageView.js
@@ -4,7 +4,7 @@
   pageView.initPage = function(activePage){
     $('nav').show();
     $('#' + activePage).show().siblings().hide();
-    $('.icon-menu3').click(function(e){
+    $('.icon-menu').click(function(e){
       $('#menu li').toggle();
     })
   };
